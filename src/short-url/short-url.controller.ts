57191e8825878692d336f9663b4c92d029ddd7c4
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2023-02-26 15:43:08 -0500

    [func] complementacao dos metodos do servico short-url
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- src/short-url/short-url.controller.ts
+++ src/short-url/short-url.controller.ts
@@ -7,26 +7,56 @@ import { UpdateShortUrlDto } from './dto/update-short-url.dto';
 export class ShortUrlController {
   constructor(private readonly shortUrlService: ShortUrlService) {}
 
+  /**
+   * 
+   * @param createShortUrlDto 
+   * @returns 
+   */
   @Post()
   create(@Body() createShortUrlDto: CreateShortUrlDto) {
     return this.shortUrlService.create(createShortUrlDto);
   }
 
+  /**
+   * 
+   * @returns 
+   */
   @Get()
   findAll() {
     return this.shortUrlService.findAll();
   }
 
+  @Get('/unique/:uniqueId') 
+  findUnique(@Param('uniqueId') uniqueId: string){
+    return this.findUnique(uniqueId);
+  }
+
+  /**
+   * 
+   * @param id 
+   * @returns 
+   */
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.shortUrlService.findOne(+id);
   }
 
+  /**
+   * 
+   * @param id 
+   * @param updateShortUrlDto 
+   * @returns 
+   */
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateShortUrlDto: UpdateShortUrlDto) {
     return this.shortUrlService.update(+id, updateShortUrlDto);
   }
 
+  /**
+   * 
+   * @param id 
+   * @returns 
+   */
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.shortUrlService.remove(+id);
--- src/short-url/short-url.service.ts
+++ src/short-url/short-url.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
 import { CreateShortUrlDto } from './dto/create-short-url.dto';
 import { UpdateShortUrlDto } from './dto/update-short-url.dto';
 import { ShortUrl } from './entities/short-url.entity';
@@ -15,38 +15,56 @@ export class ShortUrlService {
   ){}
 
   create(createShortUrlDto: CreateShortUrlDto) {
-    createShortUrlDto.uniqueId = this.generateCode();
-    return this.shortUrlRepository.save(createShortUrlDto);    //return 'This action adds a new shortUrl';
+    
+    if(createShortUrlDto.uniqueId == ''){
+      const uid = this.generateCode();
+      createShortUrlDto.uniqueId = uid;
+    } 
+
+    try {
+      return this.shortUrlRepository.save(createShortUrlDto);
+    } catch (error) {
+      if(error instanceof QueryFailedError){
+        console.log("Query failed error");
+      }
+    }
   }
 
   findAll() {
     return this.shortUrlRepository.find(); 
-    // return `This action returns all shortUrl`;
   }
 
   findOne(id: number) {
     const shortUrl = this.shortUrlRepository.findOneBy({id}); 
-    
-    this.generateCode(); 
 
     if (!shortUrl){
       throw new NotFoundException(`Short-url  ${id} not found.`);
     }
     return  shortUrl;
-    return `This action returns a #${id} shortUrl`;
   }
 
-  update(id: number, updateShortUrlDto: UpdateShortUrlDto) {
-    return `This action updates a #${id} shortUrl`;
+  update(id: number, updateShortUrlDto: UpdateShortUrlDto) {  
+    return this.shortUrlRepository.update(id, updateShortUrlDto); 
   }
 
   remove(id: number) {
-    return `This action removes a #${id} shortUrl`;
+    return this.shortUrlRepository.delete(id); 
   }
 
   generateCode(): string{
     var id = crypto.randomBytes(6).toString('base64');
     console.log(id);
     return id;
+  } 
+
+   async findUnique(uniqueId: string): Promise<ShortUrl>{
+    const shortUrl = await this.shortUrlRepository.findOne({
+      where: { uniqueId },
+    });
+
+    if(!shortUrl){
+      throw new HttpException(`Link #${uniqueId} not found`, HttpStatus.NOT_FOUND);
+    }
+    return shortUrl;
   }
 }
